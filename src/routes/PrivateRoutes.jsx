9f2fa2e921995d4757b561c4e7a01b92837ef5a4
programmerjewel <jewelruet1402@gmail.com>
2026-04-24 15:31:49 +0600

    create private route and implement role-based access control (RBAC) with private route

--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -1,11 +1,18 @@
+import Loading from "@/components/shared/Loading";
+import useRole from "@/hooks/useRole";
+import { Navigate, useLocation } from "react-router-dom";
 
 
-const PrivateRoutes = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+const PrivateRoutes = ({children, allowedRoles}) => {
+  const location = useLocation();
+  const {role, isRoleLoading} = useRole();
+
+  if(isRoleLoading) return <div className="min-h-screen flex flex-col justify-center"><Loading size="md" text="Loading..." /></div>
+  if(allowedRoles.includes(role)){
+    return children
+  }
+
+  return role ? <Navigate to= '/' replace /> : <Navigate to="/login" state={{from: location}} replace/>
 };
 
 export default PrivateRoutes;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,112 +1,168 @@
+import DashboardLayout from "@/layouts/dashboard/DashboardLayout";
+import MainLayout from "@/layouts/public/MainLayout";
+import AdminHomePage from "@/pages/dashboard/admin/AdminHomePage";
+import ManageTasksPage from "@/pages/dashboard/admin/ManageTasksPage";
+import ManageUsers from "@/pages/dashboard/admin/ManageUsersPage";
+import AddTaskPage from "@/pages/dashboard/buyer/AddTasksPage";
+import AllTasksPage from "@/pages/dashboard/buyer/AllTasksPage";
+import BuyerHomePage from "@/pages/dashboard/buyer/BuyerHomePage";
+import PaymentHistoryPage from "@/pages/dashboard/buyer/PaymentHistoryPage";
+import PurchaseCoinsPage from "@/pages/dashboard/buyer/PurchaseCoinsPage";
+import TaskDetailsPage from "@/pages/dashboard/worker/TaskDetailsPage";
+import TaskListPage from "@/pages/dashboard/worker/TaskListPage";
+import WorkerHomePage from "@/pages/dashboard/worker/WorkerHomePage";
+import WorkerSubmissionPage from "@/pages/dashboard/worker/WorkerSubmissionPage";
+import WorkerWithdrawalPage from "@/pages/dashboard/worker/WorkerWithdrawalPage";
+import LoginPage from "@/pages/public/auth/LoginPage";
+import ProfilePage from "@/pages/public/auth/ProfilePage";
+import RegisterPage from "@/pages/public/auth/RegisterPage";
+import ErrorPage from "@/pages/public/ErrorPage";
+import HomePage from "@/pages/public/Home";
+import { createBrowserRouter } from "react-router-dom";
+import PrivateRoutes from "./PrivateRoutes";
 
-import DashboardLayout from '@/layouts/dashboard/DashboardLayout';
-import MainLayout from '@/layouts/public/MainLayout';
-import AdminHomePage from '@/pages/dashboard/admin/AdminHomePage';
-import ManageTasksPage from '@/pages/dashboard/admin/ManageTasksPage';
-import ManageUsers from '@/pages/dashboard/admin/ManageUsersPage';
-import AddTaskPage from '@/pages/dashboard/buyer/AddTasksPage';
-import AllTasksPage from '@/pages/dashboard/buyer/AllTasksPage';
-import BuyerHomePage from '@/pages/dashboard/buyer/BuyerHomePage';
-import PaymentHistoryPage from '@/pages/dashboard/buyer/PaymentHistoryPage';
-import PurchaseCoinsPage from '@/pages/dashboard/buyer/PurchaseCoinsPage';
-import TaskDetailsPage from '@/pages/dashboard/worker/TaskDetailsPage';
-import TaskListPage from '@/pages/dashboard/worker/TaskListPage';
-import WorkerHomePage from '@/pages/dashboard/worker/WorkerHomePage';
-import WorkerSubmissionPage from '@/pages/dashboard/worker/WorkerSubmissionPage';
-import WorkerWithdrawalPage from '@/pages/dashboard/worker/WorkerWithdrawalPage';
-import LoginPage from '@/pages/public/auth/LoginPage';
-import ProfilePage from '@/pages/public/auth/ProfilePage';
-import RegisterPage from '@/pages/public/auth/RegisterPage';
-import ErrorPage from '@/pages/public/ErrorPage';
-import HomePage from '@/pages/public/Home';
-import { createBrowserRouter } from 'react-router-dom';
-
-const Routes= createBrowserRouter([
+const Routes = createBrowserRouter([
   {
-    path: '/',
-    element: <MainLayout/>,
-    errorElement: <ErrorPage/>,
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <ErrorPage />,
     children: [
       {
-        path: '/',
-        element: <HomePage/>,
+        path: "/",
+        element: <HomePage />,
       },
       {
-        path: '/profile',
-        element:<ProfilePage/>,
-      }
-    ]
+        path: "/profile",
+        element: <ProfilePage />,
+      },
+    ],
   },
   {
-    path: '/login',
-    element: <LoginPage/>
+    path: "/login",
+    element: <LoginPage />,
   },
   {
-    path: '/register',
-    element: <RegisterPage/>,
+    path: "/register",
+    element: <RegisterPage />,
   },
   {
-    path: '/dashboard',
-    element: <DashboardLayout/>,
+    path: "/dashboard",
+    element: (
+      <PrivateRoutes allowedRoles={["admin", "buyer", "worker"]}>
+        <DashboardLayout />
+      </PrivateRoutes>
+    ),
     children: [
       {
-        path: 'profile',
-        element: <ProfilePage/>,
+        path: "profile",
+        element: <ProfilePage />,
       },
       {
-        path: 'worker-home',
-        element: <WorkerHomePage/>,
+        path: "worker-home",
+        element: (
+          <PrivateRoutes allowedRoles={["worker"]}>
+            <WorkerHomePage />
+          </PrivateRoutes>
+        ),
       },
       {
-        path: 'task-list',
-        element: <TaskListPage/>,
+        path: "task-list",
+        element: (
+          <PrivateRoutes allowedRoles={["worker"]}>
+            <TaskListPage />
+          </PrivateRoutes>
+        ),
       },
       {
-        path: 'tasks/:id',
-        element: <TaskDetailsPage/>,
+        path: "tasks/:id",
+        element: (
+          <PrivateRoutes allowedRoles={["worker"]}>
+            <TaskDetailsPage />
+          </PrivateRoutes>
+        ),
       },
       {
-        path: 'my-submission',
-        element: <WorkerSubmissionPage/>,
+        path: "my-submission",
+        element: (
+          <PrivateRoutes allowedRoles={["worker"]}>
+            <WorkerSubmissionPage />
+          </PrivateRoutes>
+        ),
       },
       {
-        path: 'withdrawals',
-        element: <WorkerWithdrawalPage/>,
+        path: "withdrawals",
+        element: (
+          <PrivateRoutes allowedRoles={["worker"]}>
+            <WorkerWithdrawalPage />
+          </PrivateRoutes>
+        ),
       },
       {
-        path: 'buyer-home',
-        element: <BuyerHomePage/>,
+        path: "buyer-home",
+        element: (
+          <PrivateRoutes allowedRoles={["buyer"]}>
+            <BuyerHomePage />
+          </PrivateRoutes>
+        ),
       },
       {
-        path: 'add-task',
-        element: <AddTaskPage/>
+        path: "add-task",
+        element: (
+          <PrivateRoutes allowedRoles={["buyer"]}>
+            <AddTaskPage />
+          </PrivateRoutes>
+        ),
       },
       {
-        path: 'tasks',
-        element: <AllTasksPage/>,
+        path: "tasks",
+        element: (
+          <PrivateRoutes allowedRoles={["buyer"]}>
+            <AllTasksPage />
+          </PrivateRoutes>
+        ),
       },
       {
-        path: 'purchase-coins',
-        element: <PurchaseCoinsPage/>,
+        path: "purchase-coins",
+        element: (
+          <PrivateRoutes allowedRoles={["buyer"]}>
+            <PurchaseCoinsPage />
+          </PrivateRoutes>
+        ),
       },
       {
-        path: 'payments',
-        element: <PaymentHistoryPage/>,
+        path: "payments",
+        element: (
+          <PrivateRoutes allowedRoles={["buyer"]}>
+            <PaymentHistoryPage />
+          </PrivateRoutes>
+        ),
       },
       {
-        path: 'admin-home',
-        element: <AdminHomePage/>,
+        path: "admin-home",
+        element: (
+          <PrivateRoutes allowedRoles={["admin"]}>
+            <AdminHomePage />
+          </PrivateRoutes>
+        ),
       },
       {
-        path: 'manage-users',
-        element: <ManageUsers/>,
+        path: "manage-users",
+        element: (
+          <PrivateRoutes allowedRoles={["admin"]}>
+            <ManageUsers />
+          </PrivateRoutes>
+        ),
       },
       {
-        path: 'manage-tasks',
-        element: <ManageTasksPage/>,
-      }
-    ]
-  }
-])
+        path: "manage-tasks",
+        element: (
+          <PrivateRoutes allowedRoles={["admin"]}>
+            <ManageTasksPage />
+          </PrivateRoutes>
+        ),
+      },
+    ],
+  },
+]);
 
 export default Routes;
